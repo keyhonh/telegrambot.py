ec5e22915a18235771d8acc7bb2e3d95399e841a
keyhonh <hotamboyibrogimov395@gmail.com>
2026-09-06 11:44:41 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ OWNER_ID = int(os.getenv("OWNER_ID", "0") or "0")    # <-- o'z Telegram ID raqam
 REQUIRED_CHANNELS = [
     {"id": "@keyhon", "title": "📢 Asosiy kanal", "url": "https://t.me/keyhon"},
     # Kerak bo'lsa yana qo'shishingiz mumkin:
-    {"id": "-1003860693315", "title": "📢 Ikkinchi kanal", "url": "https://t.me/+3Fz0sY6URQNiNWVi"},
+    {"id": "-1003860693315", "title": "📢 Ikkinchi kanal", "url": "https://t.me/+BAvGzHhA4Oo2Y2Ji"},
 ]
 
 DB_PATH = "admin_panel.db"
